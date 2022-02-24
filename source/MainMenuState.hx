f80983c145ea778d0471e285de2bf7f67ba39d87
CerBor <58470353+CerBor@users.noreply.github.com>
2022-02-24 18:45:09 +0700

    Also bring back that cool tween thing

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -47,6 +47,8 @@ class MainMenuState extends MusicBeatState
 	var camFollow:FlxObject;
 	var camFollowPos:FlxObject;
 	var debugKeys:Array<FlxKey>;
+	var coolTween:FlxTween;
+	var coolSprite:FlxSprite;
 
 	override function create()
 	{
@@ -296,6 +298,12 @@ class MainMenuState extends MusicBeatState
 				}
 				camFollow.setPosition(spr.getGraphicMidpoint().x, spr.getGraphicMidpoint().y - add);
 				spr.centerOffsets();
+				if (coolTween != null)
+					coolTween.destroy();
+				if (coolSprite != null)
+					coolSprite.angle = spr.angle;
+				coolSprite = spr;
+				coolTween = FlxTween.angle(spr, spr.angle, -spr.angle, Conductor.crochet / 1000, {ease: FlxEase.sineInOut, type: FlxTweenType.PINGPONG});
 			}
 		});
 	}
