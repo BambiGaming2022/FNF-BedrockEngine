ce0aca4352ad6d8f299b33d741adf9a390ee5e79
Gui-iago <lucariothelast91@gmail.com>
2022-02-27 12:59:16 -0400

    fix error 2

--- source/Discord.hx
+++ source/Discord.hx
@@ -81,7 +81,7 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: 'icon',
-			largeImageText: "Engine Version: " + MainMenuState.peVersion + "\nFork Version: " + MainMenuState.beVersion,
+			largeImageText: "Engine Version: " + Main.peVersion + "\nFork Version: " + Main.beVersion,
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
